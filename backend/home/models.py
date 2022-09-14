99d899ba43d37303750e27295ba1c9923ad55cd8
Crowdbotics <engineering@crowdbotics.com>
2022-09-14 09:39:04 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class Sghjt(models.Model):
     "Generated Model"
     nbfhjf = models.BigIntegerField()
+    ugene = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
+    ghtytu = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
